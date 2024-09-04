449877c6810c635c142a7c434677d5a78472b9e4
saksham021012 <sakshamgoyal2k4@gmail.com>
2024-09-05 01:57:55 +0530

    moved footer and header outside folder and change the paths for logo and index in both header footer jsx files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Logo, Container, LogoutBtn } from '../index'
+import { Logo, Container, LogoutBtn } from './index'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
